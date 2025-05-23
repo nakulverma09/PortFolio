60e561e78782d7721aec4096d52bd4ac5f6cc734
Nakul Verma <workwithnakul09@gmail.com>
2025-05-23 12:10:15 +0530

    apply nodemailer property

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email is required'],
-    unique: true,
     lowercase: true,
     trim: true,
     validate: [validator.isEmail, 'Invalid email format'],
